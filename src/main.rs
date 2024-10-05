90e2e9ae0a3025eb0a84566e1d719d61ad9d9652
Anthony Castellano <tonycastellano13@gmail.com>
2024-10-05 09:20:25 -0400

    start parsing args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    // grab args
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() <= 1 {
+        println!("Error: An instruction must be provided");
+        exit(1);
+    }
+
+    // combine non-flag args into string
+    let prompt: String = args[1..].join(" ").to_string();
+
+    println!("{:?}", prompt)
 }
